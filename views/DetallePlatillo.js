03b7aa3a1d4da48be0be04cc6724b66882126e14
GerardoRosas <rosas.ruizlg@gmail.com>
2020-07-25 22:27:10 -0500

    Detalle de Orden

--- views/DetallePlatillo.js
+++ views/DetallePlatillo.js
@@ -1,10 +1,62 @@
-import React from 'react';
-import { Text } from 'react-native'
+import React, { useContext } from 'react';
+import { Image } from 'react-native';
+import {
+    Container, 
+    Content,
+    Footer,
+    Card,
+    FooterTab,
+    Button,
+    Body,
+    Text,
+    H1,
+    CardItem
+} from 'native-base';
+import globalStyles from '../styles/global';
+import { useNavigation } from '@react-navigation/native';
+
+import PedidoContext from '../context/pedidos/pedidosContext';
 
 const DetallePlatillo = () => {
-    return (  <Text>
-        detalle platillo
-    </Text>);
+
+    const { platillo } = useContext(PedidoContext);
+    const { nombre, imagen, descripcion, precio } = platillo;
+
+    //Redireccionar al dar clik en el boton de ordenar
+    const navigation = useNavigation();
+
+    return (
+        <Container style={globalStyles.contenedor}>
+            <Content sytyle={globalStyles.contenido}>
+                <H1 style={globalStyles.titulo}>{nombre}</H1>
+
+                <Card>
+                    <CardItem>
+                        <Body>
+                            <Image 
+                                style={globalStyles.imagen}
+                                source={{uri: imagen}}
+                            />
+                            <Text style={{marginTop: 20}}>{descripcion}</Text>
+                            <Text style={globalStyles.cantidad}>Precio: ${precio}</Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+            </Content>
+
+            <Footer>
+                <FooterTab>
+                    <Button 
+                        style={globalStyles.boton}
+                        onPress={() => navigation.navigate("FormularioPlatillo")}
+                    >
+                        <Text style={globalStyles.botonTexto}>Ordenar Platillo</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+
+        </Container>
+    );
 }
  
 export default DetallePlatillo;
--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,10 +1,86 @@
-import React from 'react';
-import { Text } from 'react-native'
+import React, { useContext, useState } from 'react';
+import {  } from 'react-native';
+import {
+    Container, 
+    Content,
+    Form,
+    Icon,
+    Input,
+    Grid,
+    Col,
+    Button,
+    Text
+
+} from 'native-base';
+import globalStyles from '../styles/global';
+import { useNavigation } from '@react-navigation/native';
+
+import PedidoContext from '../context/pedidos/pedidosContext';
 
 const FormularioPlatillo = () => {
-    return (  <Text>
-        FormularioPlatillo
-    </Text>);
+
+    //State local
+    const [ cantidad, guardarCantidad ] = useState(1);
+
+    const { platillo } = useContext(PedidoContext);
+
+    //Decrementa en uno el state 
+    const decrementarUno = () => {
+        if(cantidad > 1 ){
+            const nuevaCantidad = parseInt(cantidad) - 1;
+            guardarCantidad(nuevaCantidad);
+        }
+        return;
+    }
+
+    //incrementar en uno el state de cantidad
+    const incrementarUno = () => {
+        const nuevaCantidad = parseInt(cantidad) + 1;
+        guardarCantidad(nuevaCantidad);
+    }
+
+    
+    
+
+    return ( 
+        <Container>
+            <Content>
+                <Form>
+                    <Text style={globalStyles.titulo}>Cantidad</Text>
+
+                    <Grid>
+                        <Col>
+                            <Button
+                                dark
+                                style={{marginLeft: 60, height: 50}}
+                                onPress={() => decrementarUno()}
+                            >
+                                <Icon style={{fontSize: 30}} name="remove" />
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Input
+                                value={cantidad.toString()}
+                                style={{textAlign: 'center', fontSize:30}}
+                                keyboardType= 'numeric'
+                                onChangeText={(cantidad) => guardarCantidad(cantidad)}
+                            />
+                        </Col>
+                        <Col>
+                            <Button
+                                dark
+                                style={{marginLeft: 15, height: 50}}
+                                onPress={() => incrementarUno()}
+                            >
+                                <Icon style={{fontSize: 30}} name="add" />
+                            </Button>
+                        </Col>
+                    </Grid>
+                </Form>
+                    
+            </Content>
+        </Container>
+    );
 }
  
 export default FormularioPlatillo;
